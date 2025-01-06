9779f50e8b7611d62d9c7019ee61174f6755b124
Jules Boissier <61466984+JulesBoissier@users.noreply.github.com>
2025-01-06 20:34:56 +0000

    Simplified PoR math by shifting referential to top-left of monitor

--- src/calibration_agent.py
+++ src/calibration_agent.py
@@ -5,7 +5,19 @@ import numpy as np
 class CalibrationAgent:
 
     def __init__(self, db_path):
-        self.z_eye = 0
+        # Screen and eye depths
+        self.z_screen = 0
+        self.z_eye = 1
+
+        self.screen_params = {
+            'X_screen': -0.3,  # Top-left corner (X) in meters
+            'Y_screen': 0.2,  # Top-left corner (Y) in meters
+            'W_screen': 0.6,  # Screen width in meters
+            'H_screen': 0.4,  # Screen height in meters
+            'W_pixels': 1920,  # Screen width in pixels
+            'H_pixels': 1080   # Screen height in pixels
+        }
+
 
     def create_profile(self):
         pass
@@ -23,19 +35,6 @@ class CalibrationAgent:
             math.cos(phi) * math.sin(theta),
             math.sin(phi)
         ])
-        print(gaze_vector)
-        # Screen and eye depths
-        self.z_screen = 0
-        self.z_eye = 1
-
-        screen_params = {
-            'X_screen': 0,  # Screen center (X) in meters
-            'Y_screen': 0,  # Screen center (Y) in meters
-            'W_screen': 0.6,  # Screen width in meters
-            'H_screen': 0.4,  # Screen height in meters
-            'W_pixels': 1920,  # Screen width in pixels
-            'H_pixels': 1080   # Screen height in pixels
-        }
 
         # Handle division by zero for t
         if phi == 0:
@@ -48,20 +47,9 @@ class CalibrationAgent:
             por_x = x + t * gaze_vector[0]
             por_y = y + t * gaze_vector[1]
         
-        print("PORs:", por_x, por_y)
-        
-        # Extract screen parameters
-        X_screen = screen_params['X_screen']  # Screen center X (meters)
-        Y_screen = screen_params['Y_screen']  # Screen center Y (meters)
-        W_screen = screen_params['W_screen']  # Screen width (meters)
-        H_screen = screen_params['H_screen']  # Screen height (meters)
-        W_pixels = screen_params['W_pixels']  # Screen width (pixels)
-        H_pixels = screen_params['H_pixels']  # Screen height (pixels)
-
         # Map physical coordinates to screen pixel coordinates
-        screen_x = ((por_x - X_screen) / W_screen) * W_pixels + W_pixels / 2
-        screen_y = -((por_y - Y_screen) / H_screen) * H_pixels + H_pixels / 2
-
+        screen_x = ((por_x - self.screen_params['X_screen']) / self.screen_params['W_screen']) * self.screen_params['W_pixels']
+        screen_y = -((por_y - self.screen_params['Y_screen']) / self.screen_params['H_screen']) * self.screen_params['H_pixels']
         return screen_x, screen_y
 
 
