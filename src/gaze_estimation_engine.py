2aeb79d6908e9abfcd496ddbdabda32ae6eece83
Jules Boissier <61466984+JulesBoissier@users.noreply.github.com>
2025-01-06 20:48:01 +0000

    Added calibration step infrastructure

--- src/gaze_estimation_engine.py
+++ src/gaze_estimation_engine.py
@@ -8,6 +8,26 @@ class GazeEstimationEngine:
         self.gaze_net = gaze_net
         self.cal_agent = cal_agent
 
+    def run_calibration_steps(self, cal_steps : int = 9):
+
+        t = 0
+
+        for i in range(cal_steps):
+            
+            x_target, y_target = 0, 0
+
+            image = "uhoh"  #! How do we get an image from inside the loop?
+
+            x, y, theta, phi = self.gaze_net.predict_gaze_vector(image)
+            
+            t += self.cal_agent.calibration_step(x, y, theta, phi, x_target, y_target)
+        
+        t /= cal_steps
+
+        self.cal_agent.t = t  #! Use a setter here as opposed to accessing attribute directly.
+
+
+
     def predict_gaze_position(self, image):
         x, y, theta, phi = self.gaze_net.predict_gaze_vector(image)
         screen_n, screen_x, screen_y = self.calculate_point_of_regard(x, y, theta, phi)
