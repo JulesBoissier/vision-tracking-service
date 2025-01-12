ce1bc56720b55c1d09bff6e514963fb3ced12515
Jules Boissier <61466984+JulesBoissier@users.noreply.github.com>
2025-01-12 12:43:28 +0000

    Removed references to PreciseCalibrationAgent

--- src/calibration_agents.py
+++ src/calibration_agents.py
@@ -2,7 +2,6 @@ from typing import List, Tuple
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 import math
-import numpy as np
 
 class CalibrationAgent(ABC):
     @abstractmethod
@@ -69,58 +68,3 @@ class NaiveCalibrationAgent(CalibrationAgent):
         x_screen = self._interpolate(theta, self.calibration_map.x_values, self.calibration_map.theta_values)
         y_screen = self._interpolate(phi, self.calibration_map.y_values, self.calibration_map.phi_values)
         return x_screen, y_screen
-
-
-class PreciseCalibrationAgent(CalibrationAgent):
-
-    def __init__(self, db_path):
-
-        # TODO: Need eye position in screen space, not camera space.
-
-        self.t = 1
-        self.screen_params = {
-            'X_screen': -0.3,  # Top-left corner (X) in meters
-            'Y_screen': 0.2,  # Top-left corner (Y) in meters
-            'W_screen': 0.6,  # Screen width in meters
-            'H_screen': 0.4,  # Screen height in meters
-            'W_pixels': 1920,  # Screen width in pixels
-            'H_pixels': 1080   # Screen height in pixels
-        }
-
-    def create_profile(self):
-        pass
-    
-    def _save_profile(self):
-        pass
-
-    def load_profile(self):
-        pass
-
-    def calibration_step(self, x, y, theta, phi, x_target, y_target):
-        horizontal_t = (x - x_target) / (math.cos(phi) * math.cos(theta))
-        vertical_t = (y - y_target) / (math.cos(phi) * math.sin(theta))
-        return (horizontal_t + vertical_t) / 2
-    
-    
-
-    def calculate_point_of_regard(self, x, y, theta, phi):
-        # Calculate gaze vector
-        gaze_vector = np.array([
-            math.cos(phi) * math.cos(theta),
-            math.cos(phi) * math.sin(theta),
-            math.sin(phi)
-        ])
-
-        # Handle division by zero for t
-        if phi == 0:
-            por_x = x
-            por_y = y
-        else:
-            # Calculate screen intersection point (X, Y)
-            por_x = x + self.t * gaze_vector[0]
-            por_y = y + self.t * gaze_vector[1]
-        
-        # Map physical coordinates to screen pixel coordinates
-        screen_x = ((por_x - self.screen_params['X_screen']) / self.screen_params['W_screen']) * self.screen_params['W_pixels']
-        screen_y = -((por_y - self.screen_params['Y_screen']) / self.screen_params['H_screen']) * self.screen_params['H_pixels']
-        return screen_x, screen_y
